f92fd05daaea3f24a26b7b2a0e6f59a8eb813121
5murf <smurf-sam@hotmail.co.uk>
2014-12-14 23:35:48 +0000

    Press X to start

--- FlappyBird/FlappyBird/AppMain.cs
+++ FlappyBird/FlappyBird/AppMain.cs
@@ -35,6 +35,7 @@ namespace FlappyBird
 		private static Pickup health;
 		private static int num;
 		private static bool mGun;
+		private static bool start;
 		
 		
 		public static void Main (string[] args)
@@ -121,12 +122,12 @@ namespace FlappyBird
 			score = 0;
 			
 			bird = new Bird(gameScene);
-			
 			bullet = new Bullet(gameScene);
 			machineGun = new Pickup(-500, -500, gameScene, true);
 			health = new Pickup(-500, -500, gameScene, false);
-			
 			enemies = new List<Enemy>();
+			
+			start = false;
 			
 			for (int i = 0; i < numEnemies; i++)
 			{
@@ -150,101 +151,6 @@ namespace FlappyBird
 			var touches = Touch.GetData(0);
 			GamePadData data = GamePad.GetData(0);
 			
-			if (!bird.dead)
-			{
-				if (Input2.GamePad0.Up.Down && bird.GetPos().Y < 794 - (bird.getTexInfo().Y/2))
-					bird.Up(true);
-				
-				if (Input2.GamePad0.Down.Down && bird.GetPos().Y > 0 + (bird.getTexInfo().Y/2))
-					bird.Down(true);
-				
-				if (Input2.GamePad0.Left.Down && bird.GetPos().X > 0 + (bird.getTexInfo().X/2))
-					bird.Left(true);
-				
-				if (Input2.GamePad0.Right.Down && bird.GetPos().X < 1271 - (bird.getTexInfo().Y/2))
-					bird.Right(true);
-						
-				if (Input2.GamePad0.R.Down)
-					bullet.Shoot(bird.GetPos(), bird.getAngle(), mGun);
-			
-				for (int i = enemies.Count - 1; i >= 0 ; i--)
-				{
-					enemies[i].Update(0.0f);
-					
-				}
-				
-				bullet.Update(0.0f);
-				bird.Update(0.0f);
-				background.Update(0.0f);
-				machineGun.Update(0.0f);
-				health.Update(0.0f);
-				
-				for (int i = 0; i < enemies.Count; i++)
-				{
-					enemies[i].speed = 0.1f * (10 + (score/4));
-					if (Collision (bullet.GetBox(), enemies[i].GetBox()) && !enemies[i].dead)
-					{
-						num = rnd.Next(1, 20);
-						if (num == 3)
-						{
-							machineGun.setPos(enemies[i].GetBox().Min.X,enemies[i].GetBox().Min.Y);
-							machineGun.setVis();
-						}
-						if (num == 6)
-						{
-							health.setPos(enemies[i].GetBox().Min.X,enemies[i].GetBox().Min.Y);
-							health.setVis();
-						}
-						score = score + 1;
-						enemies[i].dead = true;
-						Enemy enemy1 = new Enemy(0, 0, gameScene);
-						enemy1.setPos();
-						enemies.Add(enemy1);
-						
-						Enemy enemy2 = new Enemy(0, 0, gameScene);
-						enemy2.setPos();
-						enemies.Add(enemy2);
-						
-						bullet.stopShoot();
-					}
-					
-					if ((Collision(enemies[i].GetBox(), bird.GetBox())) && atkDelay >= 100 && !enemies[i].dead)
-					{
-						atkDelay = 0;
-						bird.health = bird.health - 10;
-						if (bird.health == 0)
-						{
-							bird.dead = true;
-							endGameLabel.Text = ("Game Over - Score: " + score.ToString() + " Press X to restart");
-						}
-					}
-					atkDelay ++;
-				}
-				
-				if (bullet.ammo <= 0)
-				{	
-					mGun = false;
-				}
-				
-				if (mGun)
-					ammoLabel.Text = bullet.ammo.ToString();
-				else
-					ammoLabel.Text = "";
-						
-				
-			
-			if (Collision(bird.GetBox(), machineGun.GetBox()) && machineGun.checkVis())
-			{
-				machineGun.pickedUp();
-				mGun = true;
-				bullet.ammo = 40;
-			}
-			if (Collision(bird.GetBox(), health.GetBox()) && health.checkVis())
-			{
-				health.pickedUp();
-				bird.health = bird.health + 50;
-			}
-				
 			if (data.AnalogRightX > 0.2f || data.AnalogRightX < -0.2f || data.AnalogRightY > 0.2f || data.AnalogRightY < -0.2f) 
 			{
 				var angleInRadians = FMath.Atan2 (-data.AnalogRightX, -data.AnalogRightY);
@@ -252,33 +158,142 @@ namespace FlappyBird
 				bird.playerRotation = new Vector2 (-FMath.Cos (angleInRadians), -FMath.Sin (angleInRadians));
 				bird.playerMovement = new Vector2 (FMath.Cos (angleInRadians2), FMath.Sin (angleInRadians2));
 			}
-			} else
+			
+			if (start)
 			{
-				if (Input2.GamePad0.Cross.Down)
+				if (!bird.dead)
 				{
+					if (Input2.GamePad0.Up.Down && bird.GetPos().Y < 794 - (bird.getTexInfo().Y/2))
+						bird.Up(true);
+					
+					if (Input2.GamePad0.Down.Down && bird.GetPos().Y > 0 + (bird.getTexInfo().Y/2))
+						bird.Down(true);
+					
+					if (Input2.GamePad0.Left.Down && bird.GetPos().X > 0 + (bird.getTexInfo().X/2))
+						bird.Left(true);
+					
+					if (Input2.GamePad0.Right.Down && bird.GetPos().X < 1271 - (bird.getTexInfo().Y/2))
+						bird.Right(true);
+							
+					if (Input2.GamePad0.R.Down)
+						bullet.Shoot(bird.GetPos(), bird.getAngle(), mGun);
+				
+					for (int i = enemies.Count - 1; i >= 0 ; i--)
+					{
+						enemies[i].Update(0.0f);
+						
+					}
+					
+					bullet.Update(0.0f);
+					bird.Update(0.0f);
+					background.Update(0.0f);
+					machineGun.Update(0.0f);
+					health.Update(0.0f);
+					
 					for (int i = 0; i < enemies.Count; i++)
 					{
-						enemies[i].setInvis();
-					}
-					enemies.Clear();
-					for (int a = 0; a < numEnemies; a++)
-					{
-						Enemy enemy = new Enemy(rnd.Next(0, 1271), rnd.Next(0, 794), gameScene);
-						if (Collision(enemy.GetBox(),bird.GetBox2()))
-					    {
-							enemy.setPos();
+						enemies[i].speed = 0.1f * (10 + (score/4));
+						if (Collision (bullet.GetBox(), enemies[i].GetBox()) && !enemies[i].dead)
+						{
+							num = rnd.Next(1, 20);
+							if (num == 3)
+							{
+								machineGun.setPos(enemies[i].GetBox().Min.X,enemies[i].GetBox().Min.Y);
+								machineGun.setVis();
+							}
+							if (num == 6)
+							{
+								health.setPos(enemies[i].GetBox().Min.X,enemies[i].GetBox().Min.Y);
+								health.setVis();
+							}
+							score = score + 1;
+							enemies[i].dead = true;
+							Enemy enemy1 = new Enemy(0, 0, gameScene);
+							enemy1.setPos();
+							enemies.Add(enemy1);
+							
+							Enemy enemy2 = new Enemy(0, 0, gameScene);
+							enemy2.setPos();
+							enemies.Add(enemy2);
+							
+							bullet.stopShoot();
 						}
-						enemies.Add(enemy);
+						
+						if ((Collision(enemies[i].GetBox(), bird.GetBox())) && atkDelay >= 300 && !enemies[i].dead)
+						{
+							atkDelay = 0;
+							bird.health = bird.health - 10;
+							if (bird.health == 0)
+							{
+								bird.dead = true;
+								endGameLabel.Text = ("Game Over - Score: " + score.ToString() + " Press X to restart");
+							}
+						}
+						atkDelay ++;
 					}
-					bird.dead = false;
-					score = 0;
-					endGameLabel.Text = "";
-					bird.setPos(Director.Instance.GL.Context.GetViewport().Width/2,
-					Director.Instance.GL.Context.GetViewport().Height/2);
-					bird.health = 100;
-					mGun = false;
-					ammoLabel.Text = "";
 					
+					if (bullet.ammo <= 0)
+					{	
+						mGun = false;
+					}
+					
+					if (mGun)
+						ammoLabel.Text = bullet.ammo.ToString();
+					else
+						ammoLabel.Text = "";
+							
+					
+				
+				if (Collision(bird.GetBox(), machineGun.GetBox()) && machineGun.checkVis())
+				{
+					machineGun.pickedUp();
+					mGun = true;
+					bullet.ammo = 40;
+				}
+				if (Collision(bird.GetBox(), health.GetBox()) && health.checkVis())
+				{
+					health.pickedUp();
+					bird.health = bird.health + 50;
+				}
+					
+				
+				} else
+				{
+					if (Input2.GamePad0.Cross.Down)
+					{
+						for (int i = 0; i < enemies.Count; i++)
+						{
+							enemies[i].setInvis();
+						}
+						enemies.Clear();
+						for (int a = 0; a < numEnemies; a++)
+						{
+							Enemy enemy = new Enemy(rnd.Next(0, 1271), rnd.Next(0, 794), gameScene);
+							if (Collision(enemy.GetBox(),bird.GetBox2()))
+						    {
+								enemy.setPos();
+							}
+							enemies.Add(enemy);
+						}
+						bird.dead = false;
+						score = 0;
+						endGameLabel.Text = "";
+						bird.setPos(Director.Instance.GL.Context.GetViewport().Width/2,
+						Director.Instance.GL.Context.GetViewport().Height/2);
+						bird.health = 100;
+						mGun = false;
+						ammoLabel.Text = "";
+						
+					}
+				}
+			} else
+			{
+				endGameLabel.Text = "Press X to start";
+				
+				if (Input2.GamePad0.Cross.Down)
+				{
+					endGameLabel.Text = "";
+					start = true;
 				}
 			}
 			
